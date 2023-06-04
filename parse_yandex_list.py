80b6dcfe858c90e8ef856952df7b326eb2a8d5ad
silverscorpio <rajat.newtoneinstein@gmail.com>
2023-06-05 00:09:14 +0200

    words converted to lower case

--- parse_yandex_list.py
+++ parse_yandex_list.py
@@ -8,7 +8,7 @@ def clean_list(raw_list: list) -> dict:
     word_trans_dict = {}
     remove_newlines = [word.strip("\n") for word in raw_list if word != "\n"]
     for idx in range(0, len(remove_newlines), 2):
-        word_trans_dict[remove_newlines[idx]] = remove_newlines[idx + 1].lower()
+        word_trans_dict[remove_newlines[idx].lower()] = remove_newlines[idx + 1].lower()
     return word_trans_dict
 
 
