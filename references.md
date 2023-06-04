07215fa820faeac7a4003ec148706e1fbcc00d37
silverscorpio <rajat.newtoneinstein@gmail.com>
2023-06-05 00:51:19 +0200

    soup based parsing tested, refs file added

---
+++ references.md
@@ -0,0 +1,4 @@
+#### References and Resources
+
+* https://cooljugator.com/ru/
+* https://beautiful-soup-4.readthedocs.io/en/latest/#making-the-soup
---
+++ verb_aspect.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+rus_word = "говорить"
+conj_url = f"https://cooljugator.com/ru/{rus_word}"
+
+r = requests.get(conj_url)
+if r.status_code == 200:
+    resp_text = r.text
+    soup = BeautifulSoup(resp_text, features="html.parser")
+    print(soup.findAll("div", class_="ui label blue")[0].contents)
