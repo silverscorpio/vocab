3fe7b0fa764006cf26d597fec71a02b835155ac0
silverscorpio <rajat.newtoneinstein@gmail.com>
2023-06-13 02:52:35 +0530

    refactoring of app logic, begin button added, stringvar introduced

--- source/gui.py
+++ source/gui.py
@@ -7,13 +7,21 @@ from word_list import WordList
 
 class VocabGUI:
     def __init__(self):
+        # variables
         self.word_list = None
+        self.app_begin: bool = False
+        self.remember_status: bool = False
+        self.not_remember_status: bool = False
+
+        # tkinter specific variables
         # 1280 X 800
         self.root = Tk()
         self.root.title("Russian Vocab App")
         self.root.geometry(self._center_screen(win_width=700, win_height=500))
         self.root.resizable(False, False)
         self.root.configure(bg="#5B5C73")
+
+        self.word_variable = StringVar(master=self.root, value="WORDS")
 
         # style
         s = ttk.Style()
@@ -42,8 +50,16 @@ class VocabGUI:
                 16,
             ),
         )
-        # self.word = ttk.Label(self.root, text="Word", font=("Ariel", 16,))
-        self.word = self._update_insert_word(word_to_display="first")
+        self.word = ttk.Label(
+            self.root,
+            font=(
+                "Ariel",
+                16,
+            ),
+            textvariable=self.word_variable,
+        )
+        # self.word = self._update_insert_word(word_to_display="WORDS")
+        self.begin = ttk.Button(self.root, text="Begin", command=self._begin_button)
         self.remember = ttk.Button(
             self.root, text="Remember", command=self._remember_button
         )
@@ -79,6 +95,7 @@ class VocabGUI:
 
         # placing the widgets in grid
         self.date_label.grid(row=0, column=0, sticky=NW, padx=10, pady=10)
+        self.begin.grid(row=0, column=2, sticky=NE, padx=10, pady=10)
         self.timer_label.grid(row=0, column=5, sticky=NE, padx=10, pady=10)
 
         self.word.grid(row=1, column=2, padx=10, pady=10)
@@ -89,6 +106,8 @@ class VocabGUI:
         self.status_learnt.grid(row=4, column=0, sticky=SW, padx=10, pady=10)
         self.status_remaining.grid(row=5, column=0, sticky=SW, padx=10, pady=10)
         self.close.grid(row=5, column=5, sticky=SE, padx=10, pady=10)
+
+        self.root.mainloop()
 
     @staticmethod
     def date_today():
@@ -101,12 +120,12 @@ class VocabGUI:
         y_coordinate = (screen_height - win_height) // 2
         return f"{win_width}x{win_height}+{x_coordinate}+{y_coordinate}"
 
-    def run_gui(self):
-        # main event loop tkinter
-        self.root.mainloop()
-
     def _close_button(self):
         self.root.destroy()
+
+    def _begin_button(self):
+        self.app_begin = True
+        self.app_logic()
 
     def _remember_button(self):
         pass
@@ -122,16 +141,22 @@ class VocabGUI:
     # MAIN APP LOGIC
     def app_logic(self):
         self.set_word_list()
+        _word_gen = self.word_list.wd_list_generator
+        self._update_insert_word(word_to_display=next(_word_gen)[0])
 
     def _update_insert_word(self, word_to_display):
-        return ttk.Label(
-            self.root,
-            text=word_to_display,
-            font=(
-                "Ariel",
-                16,
-            ),
-        )
+        self.word_variable.set(value=word_to_display)
+
+        # print(self.word_variable.get())
+        # _word_label = ttk.Label(
+        #     self.root,
+        #     text=word_to_display,
+        #     font=(
+        #         "Ariel",
+        #         16,
+        #     ),
+        # )
+        # _word_label.grid(row=1, column=2, padx=10, pady=10)
 
     def get_wd_list(self) -> list[tuple]:
         return self.word_list.parsed_wd_list
@@ -140,4 +165,4 @@ class VocabGUI:
 if __name__ == "__main__":
     y = VocabGUI()
     y.app_logic()
-    print(y.get_wd_list())
+    # print(y.get_wd_list())
